2d5349cb684f6ba6664614db782dd116b56cf628
imuer <song.lee.vip@outlook.com>
2026-09-07 23:24:10 +0800

    初始化

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,8 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
+    // Blocked request. This host ("cp.pnt.cc.cd") is not allowed to be accessed  allowedHosts: ['cp.pnt.cc.cd']，allowedHosts: true 所有都可以访问
+    allowedHosts: true,
     // Web 形态下浏览器直连 Cloudflare 会触发 CORS 拦截，统一走本地代理转发
     proxy: {
       '/cf-api': {
